23008d38acc4d290d710ea065e1a31efecb3c15d
jd-anabi <jdanabi@protonmail.com>
2026-01-25 18:27:34 -0800

    want to estimate posterior for hopf model

--- core/app.py
+++ core/app.py
@@ -38,7 +38,7 @@ else:
     DTYPE = torch.float32
 
 if DEVICE.type == "cuda" and DTYPE == torch.float32:
-    BATCH_SIZE = 2**12
+    BATCH_SIZE = 2**15
 elif DEVICE.type == "cuda" and DTYPE == torch.float64:
     BATCH_SIZE = 2**10
 else:
@@ -58,9 +58,11 @@ def run():
     if sys.platform == "win32":
         cell_path = os.getcwd() + "\\Resources\\Cells\\"
         prior_path = os.getcwd() + "\\Resources\\Priors\\"
+        posterior_path = os.getcwd() + "\\Resources\\Posteriors\\"
     else:
         cell_path = os.getcwd() + "/Resources/Cells/"
         prior_path = os.getcwd() + "/Resources/Priors/"
+        posterior_path = os.getcwd() + "/Resources/Posteriors/"
 
     # list files in directory
     model_files = [""]
@@ -141,69 +143,75 @@ def run():
                  r"$\chi_{hb}$", r"$\chi_a$", r"$x_c$", r"$\eta_{hb}$", r"$\eta_{a}$"]
     visualizers.visualize_dist(mixed_prior, labels=hopf_labels, save_path=corner_plot_path)
 
+    hopf_posterior_path = posterior_path + "hopf_posterior.pt"
+    try:
+        posterior = torch.load(hopf_posterior_path, map_location=torch.device("cpu"))
+    except FileNotFoundError as e:
+        # --- SUMMARY STATISTICS --- #
+        num_runs = 10
+        all_training_stats = []
+        all_thetas = []
+        init_pos = torch.tensor(np.random.randint(0, 10, size=(BATCH_SIZE, 2)), dtype=DTYPE, device=DEVICE)
+        #batch_inits = helpers.concat(np.random.randint(0, 10, size=(BATCH_SIZE, 2)), np.random.randint(0, 1, size=(BATCH_SIZE, 3)))  # size: (BATCH_SIZE, 5)
+        #batch_inits = torch.tensor(batch_inits, dtype=DTYPE, device=DEVICE)
+        with torch.no_grad():
+            for _ in tqdm(range(num_runs), desc=f"Calculating summary statistics for {num_runs} runs", leave=False):
+                curr_thetas = mixed_prior.sample((BATCH_SIZE,)).to(device=DEVICE, dtype=DTYPE)
+                training_data = pipeline.gen_obs(sim="Hopf", params=curr_thetas, t=t, inits=init_pos,
+                                                 force=force[0].unsqueeze(0), n_segs=segs, steady_idx=steady_idx,
+                                                 batch_size=BATCH_SIZE, dtype=DTYPE, device=DEVICE)[0, :, :]
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore")
+                    training_stats = pipeline.gen_stats(training_data, dt)
+                    all_training_stats.append(training_stats)
+                all_thetas.append(curr_thetas)
+                del training_stats
+        summary_stats = torch.cat(all_training_stats, dim=0)
+        thetas = torch.cat(all_thetas, dim=0)
 
-    # --- SUMMARY STATISTICS --- #
-    num_runs = 15
-    all_training_stats = []
-    all_thetas = []
-    init_pos = torch.tensor(np.random.randint(0, 10, size=(BATCH_SIZE, 2)), dtype=DTYPE, device=DEVICE)
-    #batch_inits = helpers.concat(np.random.randint(0, 10, size=(BATCH_SIZE, 2)), np.random.randint(0, 1, size=(BATCH_SIZE, 3)))  # size: (BATCH_SIZE, 5)
-    #batch_inits = torch.tensor(batch_inits, dtype=DTYPE, device=DEVICE)
-    with torch.no_grad():
-        for _ in tqdm(range(num_runs), desc=f"Calculating summary statistics for {num_runs} runs", leave=False):
-            curr_thetas = mixed_prior.sample((BATCH_SIZE,)).to(device=DEVICE, dtype=DTYPE)
-            training_data = pipeline.gen_obs(sim="Hopf", params=curr_thetas, t=t, inits=init_pos,
-                                             force=force[0].unsqueeze(0), n_segs=segs, steady_idx=steady_idx,
-                                             batch_size=BATCH_SIZE, dtype=DTYPE, device=DEVICE)[0, :, :]
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                training_stats = pipeline.gen_stats(training_data, dt)
-                all_training_stats.append(training_stats)
-            all_thetas.append(curr_thetas)
-            del training_stats
-            del training_stats
-    summary_stats = torch.cat(all_training_stats, dim=0)
-    thetas = torch.cat(all_thetas, dim=0)
 
+        # --- SNPE --- #
+        # filter data
+        nan_mask = torch.isfinite(summary_stats).all(dim=1)
+        safe_magnitude_mask = (torch.abs(summary_stats) < 1e15).all(dim=1)
+        valid_idx = nan_mask & safe_magnitude_mask
+        thetas = thetas[valid_idx]
+        summary_stats = summary_stats[valid_idx]
 
-    # --- SNPE --- #
-    # filter data
-    nan_mask = torch.isfinite(summary_stats).all(dim=1)
-    safe_magnitude_mask = (torch.abs(summary_stats) < 1e15).all(dim=1)
-    valid_idx = nan_mask & safe_magnitude_mask
-    thetas = thetas[valid_idx]
-    summary_stats = summary_stats[valid_idx]
+        # set up embedded network
+        input_dim = summary_stats.shape[1]
+        output_dim = input_dim // 4
+        layer_dims = (3 * input_dim // 2, input_dim // 2)
+        embedded_net = embedded_network.EmbeddedNet(input_dim, output_dim, layer_dims)
 
-    # set up embedded network
-    input_dim = summary_stats.shape[1]
-    output_dim = input_dim // 4
-    layer_dims = (3 * input_dim // 2, input_dim // 2)
-    embedded_net = embedded_network.EmbeddedNet(input_dim, output_dim, layer_dims)
+        # set up snpe with embedded network
+        priors = []
+        prior_bounds = []
+        for vals in params.values():
+            prior_bounds.append(vals[1])
+        #steady_idx = [i for i in range(len(prior_bounds)) if i != 3]
+        #prior_bounds.pop(3)  # shape: (1, 16)
+        for curr_bounds in prior_bounds:
+            curr_prior = utils.BoxUniform(low=torch.ones(1) * curr_bounds[0], high=torch.ones(1) * curr_bounds[1])
+            priors.append(curr_prior)
+        sbi_prior = utils.MultipleIndependent(priors, device=str(DEVICE))
 
-    # set up snpe with embedded network
-    priors = []
-    prior_bounds = []
-    for vals in params.values():
-        prior_bounds.append(vals[1])
-    #steady_idx = [i for i in range(len(prior_bounds)) if i != 3]
-    #prior_bounds.pop(3)  # shape: (1, 16)
-    for curr_bounds in prior_bounds:
-        curr_prior = utils.BoxUniform(low=torch.ones(1) * curr_bounds[0], high=torch.ones(1) * curr_bounds[1])
-        priors.append(curr_prior)
-    sbi_prior = utils.MultipleIndependent(priors, device=str(DEVICE))
+        #safe_prior = sbi_prior_wrapper.SBIPriorWrapper(mixed_prior)
 
-    #safe_prior = sbi_prior_wrapper.SBIPriorWrapper(mixed_prior)
+        neural_posterior = posterior_nn(model="maf", embedding_net=embedded_net)
+        inference = SNPE(prior=sbi_prior, density_estimator=neural_posterior, device=str(DEVICE))
 
-    neural_posterior = posterior_nn(model="maf", embedding_net=embedded_net)
-    inference = SNPE(prior=sbi_prior, density_estimator=neural_posterior, device=str(DEVICE))
+        # train the density estimator
+        density_estimator = inference.append_simulations(thetas, summary_stats).train(training_batch_size=int(2**7))
 
-    # train the density estimator
-    density_estimator = inference.append_simulations(thetas, summary_stats).train(training_batch_size=int(2**7))
+        # build the posterior
+        posterior = inference.build_posterior(density_estimator)
 
-    # build the posterior
-    posterior = inference.build_posterior(density_estimator)
+        # save the posterior
+        torch.save(posterior, hopf_posterior_path)
 
     # visualize and validate posterior
+    posterior = torch.load(hopf_posterior_path, map_location=torch.device("cpu"))
     ground_truth = [row[0] for row in params]
     samples = posterior.sample((1000,), x=obs_stats)
     fig, ax = pairplot(samples.cpu().numpy(), points=np.array(ground_truth), labels=hopf_labels)
